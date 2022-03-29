109d5aaa2576b12232c14d8274b18dfce6ed4492
Scott Gould <greysilly7@gmail.com>
2022-03-29 16:47:40 -0400

    Chore(help): Forgot to run prettier

--- src/commands/General/help.ts
+++ src/commands/General/help.ts
@@ -14,22 +14,19 @@ export class StatsCommand extends Command {
 		const commands = this.container.stores.get('commands');
 		const paginatedMessage = new PaginatedMessage();
 		paginatedMessage.addPageEmbed((embed: MessageEmbed) => {
-			return embed
-				.setTitle('Commands')
-				.setDescription(
-					commands
-						.map(command => `**${command.name}** - ${command.description}`)
-						.join('\n')
-				);
+			return embed.setTitle('Commands').setDescription(commands.map((command) => `**${command.name}** - ${command.description}`).join('\n'));
 		});
 
 		return paginatedMessage.run(interaction);
 	}
 
 	registerApplicationCommands(registry: ApplicationCommandRegistry) {
-		registry.registerChatInputCommand({
-			name: this.name,
-			description: this.description
-		}, { idHints: ['958463152981409862'], guildIds: ['561218560467271681']});
+		registry.registerChatInputCommand(
+			{
+				name: this.name,
+				description: this.description
+			},
+			{ idHints: ['958463152981409862'], guildIds: ['561218560467271681'] }
+		);
 	}
 }
