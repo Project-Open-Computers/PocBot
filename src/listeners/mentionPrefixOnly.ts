cd4b727f2fe93771d2d1bfe102abdf5fcd20ba68
Scott Gould <greysilly7@gmail.com>
2022-08-10 18:01:11 -0400

    woops

--- src/listeners/mentionPrefixOnly.ts
+++ src/listeners/mentionPrefixOnly.ts
@@ -1,10 +1,9 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Listener, Events } from '@sapphire/framework';
-import { GatewayDispatchEvents } from 'discord-api-types/v10';
 import type { Message } from 'discord.js';
 
 @ApplyOptions<Listener.Options>({
-	event: GatewayDispatchEvents.MessageCreate
+	event: Events.MessageCreate
 })
 export class UserEvent extends Listener<typeof Events.MessageCreate> {
 	public async run(message: Message) {
